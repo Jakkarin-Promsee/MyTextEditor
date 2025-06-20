566b8bb9eb1e792287fed9710e325e825ab7123d
Night <jakkrinmes@gmail.com>
2025-06-20 22:58:13 +0700

    feat: add sticky properties to navbar

--- scratch/src/App.tsx
+++ scratch/src/App.tsx
@@ -8,8 +8,106 @@ function App() {
     <>
       <ThemeProvider>
         <Navbar></Navbar>
-        <h1>Hello world</h1>
-        <div className="animate-fade-in">Fade in animation</div>
+
+        <div className=" px-6 py-4">
+          <h1>Hello world</h1>
+          <div className="animate-fade-in">Fade in animation</div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            nihil soluta hic? Inventore possimus id officia ea corporis enim,
+            nemo perferendis quasi fugiat nobis cupiditate culpa sit quidem ipsa
+            doloribus, explicabo cumque quaerat iusto aperiam soluta vitae
+            adipisci ullam? Eveniet tempore itaque incidunt laborum ratione
+            ipsa, ab est pariatur vitae. Provident. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Dolor aliquam, fugit quaerat
+            laboriosam impedit recusandae nulla labore officia id nam ullam
+            velit at quia commodi inventore quidem doloremque magni accusamus
+            maxime libero temporibus! Recusandae eaque esse fugiat asperiores ab
+            ipsum? Soluta debitis obcaecati molestias nulla consequatur velit
+            provident dicta distinctio temporibus ea praesentium sit, delectus,
+            voluptatibus laborum atque doloribus expedita repellendus dolorem
+            accusamus nemo quasi quam suscipit nihil eos. Quidem perspiciatis
+            fugiat harum repellendus cupiditate mollitia accusantium omnis,
+            veritatis dolore voluptatem nihil, explicabo minima maxime
+            voluptates voluptate voluptas delectus voluptatum in alias
+            recusandae autem quisquam sunt. Voluptate in, accusantium quaerat id
+            impedit, aliquam laboriosam, voluptatibus doloribus maxime pariatur
+            atque harum ipsum. Sit quia voluptatibus enim ratione expedita
+            similique dolore blanditiis cupiditate temporibus, perspiciatis
+            magnam nemo ut est quo incidunt possimus. Suscipit earum vitae
+            maxime unde. Libero ipsum repellat itaque molestias, officiis minus
+            vitae expedita blanditiis dicta quo sunt repudiandae eveniet ad,
+            pariatur earum possimus dolore vel laboriosam nemo ipsa, natus
+            exercitationem omnis odio asperiores. Qui ipsam quas, deleniti ab
+            cum reprehenderit dolore fugit optio labore recusandae at expedita
+            officiis aperiam repudiandae. Maxime quisquam delectus ducimus
+            possimus magnam voluptatem dolorum, odio autem maiores, accusantium
+            voluptatibus eos nihil cupiditate reprehenderit explicabo
+            dignissimos. Esse doloribus consectetur nihil sed ipsam quisquam,
+            doloremque sequi totam, corrupti voluptatem sapiente odit eaque
+            laborum, culpa a quod aperiam minima rerum cumque. Nemo cum
+            molestias accusantium magni quaerat, amet animi dolore voluptatibus
+            sint cumque delectus dolorem totam facilis, maxime error numquam
+            quis porro ut iusto ea qui nam repellat quod hic? Perspiciatis
+            dolore consequatur, beatae fugiat excepturi sit ratione nam ipsam
+            amet dolorum aperiam eius, quae at ipsa corrupti nostrum ullam
+            adipisci nemo. Ducimus, accusantium quis atque libero totam
+            dignissimos? Explicabo numquam ipsa blanditiis, dicta assumenda eius
+            soluta nostrum laboriosam repellat optio ipsam iure, deleniti
+            voluptatem, sit omnis quo quibusdam voluptatum incidunt? Quis
+            ratione dolorum non deserunt adipisci facere molestias repellat
+            itaque corporis modi reprehenderit vel nobis minima facilis magnam
+            ipsa commodi laudantium, ad rerum, tenetur debitis obcaecati illo!
+            Praesentium, rerum doloribus nesciunt quia adipisci ut ratione porro
+            incidunt ipsum quis quibusdam enim animi natus eligendi sapiente
+            quaerat excepturi mollitia itaque alias! Sapiente asperiores
+            deleniti doloremque laborum eius libero placeat! Voluptates eos
+            molestiae id nobis officia molestias, ullam odio? Eos facilis
+            recusandae sunt facere quod necessitatibus, eveniet rerum
+            accusantium voluptates consequatur voluptatibus, odio illum deleniti
+            iure porro expedita quasi aut? Itaque quos ipsam ex atque! Fuga nisi
+            necessitatibus id velit accusamus quis iure quos sunt! Rem quaerat
+            modi maxime porro culpa exercitationem fugiat dolorum architecto
+            veritatis cumque aut maiores quia dolore, dignissimos ab, voluptates
+            accusamus voluptatem accusantium nostrum vero sit alias, optio natus
+            impedit. In, magni sed voluptatum quae quidem tenetur dolorem, et
+            iusto impedit, nemo dicta placeat exercitationem vitae! Accusamus
+            deleniti dicta natus eaque incidunt pariatur aliquid vitae corrupti
+            amet maxime quod perspiciatis quos ab quisquam consequuntur
+            necessitatibus minima quaerat labore ea id distinctio, expedita
+            corporis. Magni praesentium porro tempora quisquam dolorem
+            asperiores optio eum voluptate dignissimos ducimus, nihil expedita
+            provident aut. Inventore, alias ipsum? Consequatur eligendi
+            voluptatum ea reprehenderit, atque repellat ipsam odio odit
+            asperiores, cupiditate neque sit laboriosam possimus amet saepe
+            natus quae aut placeat veritatis eveniet vel minus tenetur
+            repellendus? Aliquam error dolore mollitia reprehenderit magni
+            voluptatibus esse atque autem totam exercitationem obcaecati
+            blanditiis consequatur, sit similique! Mollitia totam minus nesciunt
+            reprehenderit beatae sunt dicta, fuga quasi aspernatur repellat
+            labore delectus impedit temporibus autem ratione? Distinctio tenetur
+            alias nesciunt inventore placeat esse, sequi hic officia soluta
+            nostrum excepturi aliquid corporis, debitis dolorum repellendus,
+            blanditiis minus quo. Enim earum quae nemo cupiditate ducimus soluta
+            placeat neque sequi dolor reiciendis recusandae provident ratione
+            voluptates maiores modi vel voluptatibus voluptatum asperiores nisi,
+            unde dolores! Corrupti voluptates, officia quis ab rerum
+            voluptatibus fugiat, ducimus omnis, similique libero eligendi?
+            Voluptatem, corrupti voluptatibus necessitatibus vitae molestiae,
+            distinctio, enim atque ipsa velit quos earum aspernatur aliquid
+            eveniet natus nisi molestias. In est aliquam voluptatibus quis quasi
+            dolorum asperiores saepe odit, sed illo officia quas doloremque
+            explicabo veniam cumque omnis animi blanditiis accusamus optio
+            deserunt hic nobis nesciunt sapiente quam! Amet ipsa, et porro
+            necessitatibus, sint temporibus numquam tenetur ea quaerat
+            laudantium nostrum. Nesciunt perspiciatis magni sint quasi dolores
+            praesentium ipsa minus recusandae iure rem consequuntur fugit sit
+            atque ducimus necessitatibus eos et quia nam, rerum neque incidunt
+            ullam! Quasi, illo. Nihil tempore harum illum quaerat accusantium
+            excepturi ratione optio, suscipit vitae facilis!
+          </p>
+        </div>
+
         <Footer></Footer>
       </ThemeProvider>
     </>
--- scratch/src/components/Navbar.tsx
+++ scratch/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { ThemeToggle } from "./ThemeToggle";
 
 export const Navbar = () => {
   return (
-    <nav className="w-full px-6 py-4 shadow-md bg-white dark:bg-gray-900 dark:text-white">
+    <nav className="w-full px-6 py-4 shadow-md sticky top-0 z-50 bg-white dark:bg-gray-900 dark:text-white">
       <div className="max-w-screen-xl mx-auto flex justify-between items-center">
         <h1 className="text-xl font-bold text-primary-light dark:text-primary-dark">
           Medium Editor

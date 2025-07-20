bd11e555e3544ff242875574d5f55b33919f5d04
Night <jakkrinmes@gmail.com>
2025-07-20 23:25:09 +0700

    feat: update footer

--- scratch/src/components/Page/Footer.tsx
+++ scratch/src/components/Page/Footer.tsx
@@ -1,5 +1,5 @@
 export const Footer = () => (
   <footer className="bg-secondary text-secondary-foreground flex flex-col justify-center text-center py-4 mt-auto text-sm animate-colors">
-    <p>© {new Date().getFullYear()} Medium Editor. All rights reserved.</p>
+    <p>© {new Date().getFullYear()} My Editor. All rights reserved.</p>
   </footer>
 );
